5ab15511928344d89db3a8a8d24b049ee087f3ee
MWLeonel <wmleonel6@gmail.com>
2020-10-01 13:38:25 -0300

    Par ou Ímpar

---
+++ ParImpar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"O número digitado {numero} é PAR!");
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"O número digitado {numero} é ÍMPAR!");
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
